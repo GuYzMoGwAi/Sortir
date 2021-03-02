954ad3f72a43fb96e368e4462fd0e5367285d63c
amuratet <amilcar.muratet@gmail.com>
2021-03-02 18:54:13 +0100

    [pageAccueil] pseudo liste de sorties + css

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -14,18 +14,65 @@ class AccueilController extends AbstractController
     public function accueil(): Response
     {
         return $this->render('accueil/accueil.html.twig', [
-            '1' => [
-                'nom' => 'Au grand air',
-                'date' => '28/06/2021',
-                'cloture' => '21/06/2021',
-                'inscrits' => 8,
-                'places' => 10,
-                'etat' => 'En cours',
-                'inscrit' => true,
-                'organisateur' => 'John Orga',
-                'action' => [
-                    'inscription' => false,
-                    'desister' => true,
+            'userCookie' => [
+                'nom' => 'John Doe'
+            ],
+            'sorties' => [
+                'sortie1' => [
+                    'nom' => 'Au grand air',
+                    'date' => '28/06/2021',
+                    'cloture' => '21/06/2021 20h30',
+                    'inscrits' => 8,
+                    'places' => 10,
+                    'etat' => 'En cours',
+                    'inscrit' => true,
+                    'organisateur' => 'John Orga',
+                    'action' => [
+                        'inscription' => false,
+                        'desister' => true,
+                    ],
+                ],
+                'sortie2' => [
+                    'nom' => 'À la mer',
+                    'date' => '25/06/2021',
+                    'cloture' => '18/06/2021 11h20',
+                    'inscrits' => 8,
+                    'places' => 10,
+                    'etat' => 'En cours',
+                    'inscrit' => false,
+                    'organisateur' => 'John Doe',
+                    'action' => [
+                        'inscription' => false,
+                        'desister' => true,
+                    ],
+                ],
+                'sortie3' => [
+                    'nom' => 'Virus sharing',
+                    'date' => '28/06/2021',
+                    'cloture' => '21/06/2021 16h40',
+                    'inscrits' => 8,
+                    'places' => 10,
+                    'etat' => 'En cours',
+                    'inscrit' => true,
+                    'organisateur' => 'Imperator Macron',
+                    'action' => [
+                        'inscription' => false,
+                        'desister' => true,
+                    ],
+                ],
+                'sortie4' => [
+                    'nom' => 'Tir au chat',
+                    'date' => '28/06/2021',
+                    'cloture' => '21/06/2021 16h40',
+                    'inscrits' => 6,
+                    'places' => 8,
+                    'etat' => 'En cours',
+                    'inscrit' => true,
+                    'organisateur' => 'John Doe',
+                    'action' => [
+                        'inscription' => false,
+                        'desister' => true,
+                    ],
                 ],
             ],
         ]);

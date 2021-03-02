38bba33f5cf69e97fbdbd08d65be6c576e2fbe11
GuYzMoGwAi <blaudlewis@hotmail.fr>
2021-03-02 17:31:39 +0100

    [pageInscription] html inscription

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -17,28 +17,6 @@ class InscriptionController extends AbstractController
      */
     public function inscription(Request $request )
     {
-        // 1) Construire le formulaire
-        $user = new User();
-        $form = $this->createForm(userType::class, $user);
-
-        // 2) gérer l'envoie ( que sur le POST )
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            // 4) Sauvegarde l'utilisateur
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // todo faire tout autre traitement - comme l'envoie de mail.
-            // todo peut-être définir un message de réussite "flash" pour l'utilisateur
-
-            return $this->redirectToRoute('inscription');
-        }
-
-        return $this->render(
-            'utilisateurs/inscription.html.twig',
-            array('form' => $form->createView())
-        );
+        return $this->render('utilisateurs/inscription.html.twig');
     }
 }

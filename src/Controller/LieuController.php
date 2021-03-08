d475217e2890500e5b267c321f3c7301165f52ee
GuYzMoGwAi <blaudlewis@hotmail.fr>
2021-03-08 23:53:01 +0100

    [pageSite] first commit

--- src/Controller/LieuController.php
+++ src/Controller/LieuController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Lieu;
-use App\Entity\Ville;
+use App\Form\LieuType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,20 +13,24 @@ class LieuController extends AbstractController
 {
     /**
      * @Route("/lieu", name="lieu")
+     * @param Request $request
+     * @return Response
      */
-    public function lieu(): Response
+    public function lieu(Request $request): Response
     {
+        $lieu = new Lieu();
+        $lieux = $this->getDoctrine()->getRepository(Lieu::class)->findAll();
+        $form = $this->createForm(LieuType::class, $lieu);
+        $form->handleRequest($request);
+//        dd($lieux);
 
-        $ville = $this->getDoctrine()->getRepository(Ville::class);
-        $lieux = $this->getDoctrine()->getRepository(Lieu::class);
-
-        if (!$ville && $lieux){
+        if (!$lieux && $form) {
             throw $this->createNotFoundException("Ville ou lieu non trouvé");
         }
 
-        return $this->render('lieu/lieu.html.twig', array(
-            'villes' => $ville,
+        return $this->render('lieu/lieu.html.twig', [
             'lieux' => $lieux,
-        ));
+            'lieuForm' => $form->createView(),
+        ]);
     }
 }
